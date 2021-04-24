75c5074e3e74d5820db6ca2b1b824854f7819e65
a.cherevko <a.cherevko@disoft.dev>
2021-04-24 18:27:34 +0300

    finished movie model + migration

--- movies/models.py
+++ movies/models.py
@@ -7,3 +7,6 @@ class Movie(models.Model):
     name = models.CharField(max_length=255, default='', blank=True, verbose_name='Название фильма')
     release_date = models.DateField(null=True)
     duration = models.DurationField(null=True)
+    description = models.TextField(default='', blank=True)
+    rating = models.FloatField(null=True)
+
