e17e2fe70afa74f3e64058b0d3a4549bbbf6d133
a.cherevko <a.cherevko@disoft.dev>
2021-04-24 17:45:50 +0300

    created permissions, added permissions to directory views

---
+++ core/permissions.py
@@ -0,0 +1,23 @@
+from rest_framework import permissions
+
+from core.models import User
+
+
+class IsUserPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.user_type == User.UserTypeChoices.user
+
+
+class IsAdminPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.user_type == User.UserTypeChoices.admin
+
+
+class IsDirectorPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.user_type == User.UserTypeChoices.director
+
+
+class IsStaffPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_staff
