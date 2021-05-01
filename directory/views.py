f0cd9bcc2cdea4a37b1074acfb7d1e5645e9c85b
a.cherevko <a.cherevko@disoft.dev>
2021-05-01 17:03:09 +0300

    fixed directory viewsets permissions, added exception if admin is trying to create director

--- directory/views.py
+++ directory/views.py
@@ -8,22 +8,22 @@ from directory import serializers, models
 class ActorViewSet(view_mixins.StaffEditPermissionViewSet):
     queryset = models.Actor.objects.all()
     serializer_class = serializers.ActorSerializer
-    permission_classes = (rest_framework.permissions.IsAuthenticated, permissions.IsStaffPermission)
+    permission_classes = (rest_framework.permissions.IsAuthenticated, permissions.IsAdminPermission)
 
 
 class ActorMovieViewSet(view_mixins.StaffEditPermissionViewSet):
     queryset = models.ActorMovie.objects.all()
     serializer_class = serializers.ActorMovieSerializer
-    permission_classes = (rest_framework.permissions.IsAuthenticated, permissions.IsStaffPermission)
+    permission_classes = (rest_framework.permissions.IsAuthenticated, permissions.IsAdminPermission)
 
 
 class MovieDirectorViewSet(view_mixins.StaffEditPermissionViewSet):
     queryset = models.MovieDirector.objects.all()
     serializer_class = serializers.MovieDirectorSerializer
-    permission_classes = (rest_framework.permissions.IsAuthenticated, permissions.IsStaffPermission)
+    permission_classes = (rest_framework.permissions.IsAuthenticated, permissions.IsAdminPermission)
 
 
 class MovieGenreViewSet(view_mixins.StaffEditPermissionViewSet):
     queryset = models.MovieGenre.objects.all()
     serializer_class = serializers.MovieGenreSerializer
-    permission_classes = (rest_framework.permissions.IsAuthenticated, permissions.IsStaffPermission)
+    permission_classes = (rest_framework.permissions.IsAuthenticated, permissions.IsAdminPermission)
