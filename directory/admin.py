4e26ccfde6edc32a4e420907e88b9a342ad8d325
a.cherevko <a.cherevko@disoft.dev>
2021-05-03 13:01:58 +0300

    added directory and movies models to django admin

--- directory/admin.py
+++ directory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from directory import models
+
+admin.site.register(models.Actor)
+admin.site.register(models.ActorMovie)
+admin.site.register(models.MovieDirector)
+admin.site.register(models.MovieGenre)
--- movies/models.py
+++ movies/models.py
@@ -7,3 +7,7 @@ class Movie(models.Model):
     name = models.CharField(max_length=255, default='', blank=True, verbose_name='Название фильма')
     release_date = models.DateField(null=True)
     duration = models.DurationField(null=True)
+
+    class Meta:
+        verbose_name = 'Кино'
+        verbose_name_plural = 'Кино'
